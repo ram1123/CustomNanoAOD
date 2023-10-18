b46730fc27b48066c289578a90d5490f534e6801
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2023-10-18 18:10:07 +0200

    First commit

---
+++ GetCustomNanoAODSet.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+#############################################################
+#   This script is used by McM when it performs automatic   #
+#  validation in HTCondor or submits requests to computing  #
+#                                                           #
+#      !!! THIS FILE IS NOT MEANT TO BE RUN BY YOU !!!      #
+# If you want to run validation script yourself you need to #
+#     get a "Get test" script which can be retrieved by     #
+#  clicking a button next to one you just clicked. It will  #
+# say "Get test command" when you hover your mouse over it  #
+#      If you try to run this, you will have a bad time     #
+#############################################################
+
+cd /afs/cern.ch/cms/PPD/PdmV/work/McM/submit/HIG-RunIISummer20UL18NanoAODv9-02546/
+
+# Make voms proxy
+voms-proxy-init --voms cms --out $(pwd)/voms_proxy.txt --hours 4
+export X509_USER_PROXY=$(pwd)/voms_proxy.txt
+
+export SCRAM_ARCH=slc7_amd64_gcc700
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [ -r CMSSW_10_6_26/src ] ; then
+  echo release CMSSW_10_6_26 already exists
+else
+  scram p CMSSW CMSSW_10_6_26
+fi
+cd CMSSW_10_6_26/src
+eval `scram runtime -sh`
+
+scram b
+cd ../..
+
+# Maximum validation duration: 28800s
+# Margin for validation duration: 30%
+# Validation duration with margin: 28800 * (1 - 0.30) = 20160s
+# Time per event for each sequence: 0.4300s
+# Threads for each sequence: 2
+# Time per event for single thread for each sequence: 2 * 0.4300s = 0.8600s
+# Which adds up to 0.8600s per event
+# Single core events that fit in validation duration: 20160s / 0.8600s = 23441
+# Produced events limit in McM is 10000
+# According to 1.0000 efficiency, validation should run 10000 / 1.0000 = 10000 events to reach the limit of 10000
+# Take the minimum of 23441 and 10000, but more than 0 -> 10000
+# It is estimated that this validation will produce: 10000 * 1.0000 = 10000 events
+EVENTS=10000
+
+
+# cmsDriver command
+# cmsDriver.py  --python_filename HIG-RunIISummer20UL18NanoAODv9-02546_1_cfg.py --eventcontent NANOEDMAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --fileout file:HIG-RunIISummer20UL18NanoAODv9-02546.root --conditions 106X_upgrade2018_realistic_v16_L1v1 --step NANO --filein "dbs:/GluGluToRadionToHHTo2G2WTo2G4Q_M-1000_TuneCP5_PSWeights_narrow_13TeV-madgraph-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM" --era Run2_2018,run2_nanoAOD_106Xv2 --no_exec --mc -n $EVENTS || exit $? ;
+
+# Run generated config
+REPORT_NAME=HIG-RunIISummer20UL18NanoAODv9-02546_report.xml
+# Run the cmsRun
+cmsRun -e -j $REPORT_NAME HIG-RunIISummer20UL18NanoAODv9-02546_1_cfg.py || exit $? ;
+
+# Parse values from HIG-RunIISummer20UL18NanoAODv9-02546_report.xml report
+processedEvents=$(grep -Po "(?<=<Metric Name=\"NumberEvents\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+producedEvents=$(grep -Po "(?<=<TotalEvents>)(\d*)(?=</TotalEvents>)" $REPORT_NAME | tail -n 1)
+threads=$(grep -Po "(?<=<Metric Name=\"NumberOfThreads\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+peakValueRss=$(grep -Po "(?<=<Metric Name=\"PeakValueRss\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+peakValueVsize=$(grep -Po "(?<=<Metric Name=\"PeakValueVsize\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+totalSize=$(grep -Po "(?<=<Metric Name=\"Timing-tstoragefile-write-totalMegabytes\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+totalSizeAlt=$(grep -Po "(?<=<Metric Name=\"Timing-file-write-totalMegabytes\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+totalJobTime=$(grep -Po "(?<=<Metric Name=\"TotalJobTime\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+totalJobCPU=$(grep -Po "(?<=<Metric Name=\"TotalJobCPU\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+eventThroughput=$(grep -Po "(?<=<Metric Name=\"EventThroughput\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+avgEventTime=$(grep -Po "(?<=<Metric Name=\"AvgEventTime\" Value=\")(.*)(?=\"/>)" $REPORT_NAME | tail -n 1)
+if [ -z "$threads" ]; then
+  echo "Could not find NumberOfThreads in report, defaulting to 1"
+  threads=1
+fi
+if [ -z "$eventThroughput" ]; then
+  eventThroughput=$(bc -l <<< "scale=4; 1 / ($avgEventTime / $threads)")
+fi
+if [ -z "$totalSize" ]; then
+  totalSize=$totalSizeAlt
+fi
+if [ -z "$processedEvents" ]; then
+  processedEvents=$EVENTS
+fi
+echo "Validation report of HIG-RunIISummer20UL18NanoAODv9-02546 sequence 1/1"
+echo "Processed events: $processedEvents"
+echo "Produced events: $producedEvents"
+echo "Threads: $threads"
+echo "Peak value RSS: $peakValueRss MB"
+echo "Peak value Vsize: $peakValueVsize MB"
+echo "Total size: $totalSize MB"
+echo "Total job time: $totalJobTime s"
+echo "Total CPU time: $totalJobCPU s"
+echo "Event throughput: $eventThroughput"
+echo "CPU efficiency: "$(bc -l <<< "scale=2; ($totalJobCPU * 100) / ($threads * $totalJobTime)")" %"
+echo "Size per event: "$(bc -l <<< "scale=4; ($totalSize * 1024 / $producedEvents)")" kB"
+echo "Time per event: "$(bc -l <<< "scale=4; (1 / $eventThroughput)")" s"
+echo "Filter efficiency percent: "$(bc -l <<< "scale=8; ($producedEvents * 100) / $processedEvents")" %"
+echo "Filter efficiency fraction: "$(bc -l <<< "scale=10; ($producedEvents) / $processedEvents")
---
+++ README.md
@@ -0,0 +1,12 @@
+# Setup
+
+1. Use the script [GetCustomNanoAODSet.sh](GetCustomNanoAODSet.sh) to setup the official nanoAOD production for the UL18 campaign.
+    - Above script is obtained from the following sources:
+        - nanoAOD prepID: [https://cms-pdmv.cern.ch/mcm/requests?prepid=HIG-RunIISummer20UL18NanoAODv9-02546&page=0&shown=127](https://cms-pdmv.cern.ch/mcm/requests?prepid=HIG-RunIISummer20UL18NanoAODv9-02546&page=0&shown=127)
+            - `.sh` script: [https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_setup/HIG-RunIISummer20UL18NanoAODv9-02546](https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_setup/HIG-RunIISummer20UL18NanoAODv9-02546)
+
+
+```bash
+bash GetCustomNanoAODSet.sh
+
+```
